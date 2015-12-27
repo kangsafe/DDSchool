a80a668a7e53dc9f3bafd9388361b6fbc70712e3
kangsafe <kangsafe@hotmail.com>
2015-12-27 12:32:26 +0800

    通知公告

--- app/src/main/java/com/ddschool/activity/NoticeActivity.java
+++ app/src/main/java/com/ddschool/activity/NoticeActivity.java
@@ -41,8 +41,8 @@ public class NoticeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_notice);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
         // Create the adapter that will return a fragment for each of the three
         // primary sections of the activity.
         mSectionsPagerAdapter = new SectionsPagerAdapter(getSupportFragmentManager());
