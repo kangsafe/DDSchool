cb8c8ee4e0a7bc54eba0d4c620ebddd4067bd87d
kangsafe <kangsafe@hotmail.com>
2016-01-06 22:34:32 +0800

    Merge branch 'master' of https://github.com/kangsafe/DDSchool
    Conflicts:
    	app/src/main/java/com/ddschool/activity/BaseActivity.java
    	app/src/main/java/com/ddschool/activity/LoginActivity.java
    	app/src/main/java/com/ddschool/activity/MainActivity.java
    	app/src/main/java/com/ddschool/jpush/JPushReceiver.java

--- app/src/main/java/com/ddschool/app/BaseApplication.java
+++ app/src/main/java/com/ddschool/app/BaseApplication.java
@@ -1,7 +1,13 @@
 package com.ddschool.app;
 
 import android.app.Application;
+import android.app.Notification;
 import android.util.Log;
+
+import com.ddschool.R;
+
+import cn.jpush.android.api.BasicPushNotificationBuilder;
+import cn.jpush.android.api.CustomPushNotificationBuilder;
 import cn.jpush.android.api.JPushInterface;
 
 /**
@@ -19,5 +25,19 @@ public class BaseApplication extends Application {
          
          JPushInterface.setDebugMode(true); 	// 设置开启日志,发布时请关闭日志
          JPushInterface.init(this);     		// 初始化 JPush
+
+        BasicPushNotificationBuilder builder1 = new BasicPushNotificationBuilder(this);
+        builder1.statusBarDrawable = R.mipmap.arrow_up;
+        //设置为自动消失
+        builder1.notificationFlags = Notification.FLAG_AUTO_CANCEL;
+        // 设置为铃声与震动都要
+        builder1.notificationDefaults = Notification.DEFAULT_SOUND;// ｜ Notification.DEFAULT_VIBRATE | Notification.DEFAULT_LIGHTS;
+        JPushInterface.setPushNotificationBuilder(1, builder1);
+        // 指定定制的 Notification Layout
+        CustomPushNotificationBuilder builder2 = new CustomPushNotificationBuilder(this,
+                R.layout.activity_login, R.id.icon, R.id.title, R.id.text);
+        builder2.statusBarDrawable = R.mipmap.ic_launcher;      // 指定最顶层状态栏小图标
+        builder2.layoutIconDrawable = R.mipmap.ic_launcher1;   // 指定下拉状态栏时显示的通知图标
+        JPushInterface.setPushNotificationBuilder(2, builder2);
     }
 }
